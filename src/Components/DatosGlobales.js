7b35e204284fbf66892eb0658c5dd520d2d54c3a
Guille <guiller22>
2021-02-18 20:44:13 +0100

    s

--- src/Components/DatosGlobales.js
+++ src/Components/DatosGlobales.js
@@ -1,36 +1,35 @@
-import React, {useState, useEffect} from 'react';
+import React, {
+  useState,
+  useEffect
+} from 'react';
 import axios from 'axios';
-const peticionGet=async()=>{
-      
+const Datos_Globales = () => {
+  const peticionGet = async () => {
 
-    const baseUrl="http://localhost:4006/vacunas/";
-    const [data, setData]=useState([]);
-    const[contador,setContador]=useState(0);
-    const[dosisEntregadas,setDosisEntregadas]=useState(0);
-    const[dosisAdministradas,setDosisAdministradas]=useState(0);
-    const[personasPauta,setPersonasPauta]=useState(0);
-    
-  response.data.map(vacuna =>){
-    contador1++
-    
-    d_entregadas=d_entregadas+vacuna.do
-  }
+    const baseUrl = "http://localhost:4006/vacunas/";
+    const [data, setData] = useState([]);
+    /*const [contador, setContador] = useState(0);
+    const [dosisEntregadas, setDosisEntregadas] = useState(0);
+    const [dosisAdministradas, setDosisAdministradas] = useState(0);
+    const [personasPauta, setPersonasPauta] = useState(0);*/
+
+
     await axios.get(baseUrl)
-    .then(response=>{
-      setData(response.data);
-      //console.log(response.data);
-    }).catch(error=>{
-      console.log(error);
-    })
-  }//peticionGe
-  useEffect(() =>{
-      peticionGet();
+      .then(response => {
+        setData(response.data);
+        //console.log(response.data);
+      }).catch(error => {
+        console.log(error);
+      })
+  } //peticionGet
+  useEffect(() => {
+    peticionGet();
   }, [])
-  return(
-    <div className="App">
-        <h1>Datos Globales</h1>
-    </div>
-)
+  return ( <div className = "App" > <h1> Datos Globales </h1> </div>
+
+  )
+}
+
 
 
 export default Datos_Globales;
